e4aaf673abdf820efe33b57f1fb4e87440d5d106
james.c-hernandez <James.Hernandez@outlook.com>
2023-09-10 20:35:38 -0400

    added hero page template

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ app.set('view engine', 'ejs');
 const name = 'james';
 console.log(name);
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     //res.sendFile('./views/index.js', { root: __dirname })
     res.render('index');
+})
+
+app.get('/hero', (req, res) => {
+    //res.sendFile('./views/index.js', { root: __dirname })
+    res.render('hero');
 })
